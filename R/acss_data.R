b0f9472e3f18d83df0aa1b2c49b9668fc63db9fb
Henrik Singmann <henrik.singmann (at) psychologie.uni-freiburg.de>
2013-12-10 22:15:39 +0100

    added documentation for data set

---
+++ R/acss_data.R
@@ -0,0 +1,10 @@
+#' @name acss_data
+#' @title acss: algorithmic complexity of short strings
+#' @description Contains the algorithmic complexity for short string, an approximation of the Kolmogorov Complexity of a short string using the coding theorem method.
+#' 
+#' This dataset shouldn'T be called directly but rather through the acessor functions such as \code{\link{acss}}.
+#' @docType data
+#' @usage acss_data
+#' @author Nicolas Gauvrit, Fernando Soler Toscano, and Hector Zenil. Ported to R by Henrik Singmann.
+#' 
+NULL
